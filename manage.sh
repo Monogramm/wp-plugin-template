bf1110a5bf30910286e0b9b633c463ccf24eafa2
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-04-19 15:30:25 +0200

    :sparkles: Update Manage script with volumes
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- manage.sh
+++ manage.sh
@@ -92,8 +92,7 @@ case "${1}" in
     prepare_release "${@:2}";;
 
     # DEV env
-    start) docker-compose up -d "${@:2}"
-    chown 'www-data:www-data' -R '/srv/wordpress/html/wp-content';;
+    start) docker-compose up -d "${@:2}";;
     stop) docker-compose down "${@:2}";;
     test) set -e
     docker-compose build
@@ -109,8 +108,7 @@ case "${1}" in
     docker-compose down -v
     set +e;;
     logs) docker-compose logs -f "${@:2}";;
-    reset) docker-compose down
-    rm -rf /srv/wordpress;;
+    reset) docker-compose down -v;;
     sut) docker-compose run -T sut "${@:2}";;
     phpcbf) docker-compose run -T sut ./vendor/bin/phpcbf;;
     wp) docker-compose exec -T --user www-data wordpress wp "${@:2}";;
